7c206e8f2b7f1a2068e92519a3bf08b9294658ed
Hao Chen <hao.chen23@outlook.com>
2022-04-16 20:19:36 +1200

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var router = express.Router();
+var pjson = require("../package.json");
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: pjson.name });
+});
+
+module.exports = router;
